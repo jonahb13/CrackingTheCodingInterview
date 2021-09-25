a553dbf69cd49a88a749a1883cfc42b487c777a1
Jonah Beers <jbeers013@gmail.com>
2021-09-25 16:36:53 -0400

    Binary search tree implementation

--- practice/trees_and_graphs/binary_search_tree.py
+++ practice/trees_and_graphs/binary_search_tree.py
@@ -2,6 +2,7 @@
 Binary Search Tree
 
 Balanced binary search trees have O(log(n)) time complexity for inserting and finding nodes.
+(This is not a self balancing binary search tree, so worst case is O(n))
 """
 
 class Node:
@@ -10,6 +11,7 @@ class Node:
         self.left = None
         self.data = data
         self.right = None
+        
         
 class BinarySearchTree:
     
@@ -36,6 +38,24 @@ class BinarySearchTree:
                 self.add_node(data, node.right)
             else:
                 node.right = Node(data)
+                
+    def in_order_traversal(self, node: Node):
+        if node:
+            self.in_order_traversal(node.left)
+            print(node.data, end=" ")
+            self.in_order_traversal(node.right)
+    
+    def pre_order_traversal(self, node: Node):
+        if node:
+            print(node.data, end=" ")
+            self.pre_order_traversal(node.left)
+            self.pre_order_traversal(node.right)
+
+    def post_order_traversal(self, node: Node):
+        if node:
+            self.post_order_traversal(node.left)
+            self.post_order_traversal(node.right)
+            print(node.data, end=" ")
     
     def find(self, data):
         if self.root is None:
@@ -60,4 +80,9 @@ tree.add(1)
 tree.add(3)
 tree.add(5)
 tree.add(7)
-        
+print('In-Order Traversal of tree:', end=' ') 
+tree.in_order_traversal(tree.root)
+print('\nPre-Order Traversal of tree:', end=' ')
+tree.pre_order_traversal(tree.root)
+print('\nPost-Order Traversal of tree:', end=' ')
+tree.post_order_traversal(tree.root)
